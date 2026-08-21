c9f8c37193cb289834e02aa9d7941fd5991eb5e0
ROBSON LAURENTINO SANT'ANA <robson_l_santana@estudante.sesisenai.org.br>
2026-08-20 21:26:23 -0300

    feat: adiciona o componente Hero

--- landing-page/src/components/Hero/index.tsx
+++ landing-page/src/components/Hero/index.tsx
@@ -1,15 +1,36 @@
 import { MdOutlinePets } from "react-icons/md";
-
-export default function Hero (){
-    return(
+import { FaStar } from 'react-icons/fa'
+export default function Hero() {
+    return (
         <section className='items-center flex justify-center flex-col'>
-            <div className='flex gap-2 mt-10 bg-[#1f9271] p-4 rounded-full'>
-                <MdOutlinePets size={24} color="#3F9271" />
-                <p>Feito para tutores atentos</p>
+            <div className='flex gap-2 items-center mt-10 md:mt-20 bg-[#DCEFE4] p-2 rounded-full'>
+                <MdOutlinePets size={20} color="#3F9271" />
+                <p className='text-[12px] md:text-xs text-[#153229] font-display'>Feito para tutores atentos</p>
             </div>
-            <div className='text-center justify-center mt-15  items-center'>
-                <h1 className='font-black text-[#153229] text-4xl'>Toda a rotina do seu pet</h1>
-                <h1 className='font-black text-[#FF6B4A] text-4xl'>numa coleira só.</h1>
+            <div className='text-center justify-center mt-5 items-center w-xs md:w-md'>
+                <h1 className='font-black text-[#153229] text-3xl md:text-4xl'>Toda a rotina do seu pet,</h1>
+                <h1 className='font-black text-[#FF6B4A] text-3xl md:text-4xl'>numa coleira só.</h1>
+                <p className='text-wrap mt-4 mb-6 text-[#4B5A54] text-sm md:text-base'>Agende consultas, acompanhe vacinas e fale com veterinários sem sair do app. O PetCare organiza o que seu pet precisa, antes de você precisar lembrar.</p>
+                <div className='flex justify-center gap-5'>
+                    <a href='' className='text-bold text-xs md:text-sm p-2 text-white rounded-full bg-[#FF6B4A] shadow-2xl '>Ver Funcionalidades</a>
+                    <a href='' className='text-bold text-xs md:text-sm p-2 border rounded-full border-[#ccc] shadow-2xl'>Falar com um veterinário</a>
+                </div>
+                <div className='flex justify-center gap-8 mt-10 items-center content-center'>
+                    <div className='flex flex-col gap-2 justify-center items-center'>
+                        <p className='font-bold'>12 mil+</p>
+                        <p className='text-[#4B5A54] text-xs md:text-sm'>pets cadastrados</p>
+                    </div>
+                    <div className='flex-col flex gap-2 justify-center items-center '>
+                        <div className='flex gap-2 justify-center items-center'>
+                            <p className='font-bold'>4.9 </p> <FaStar />
+                        </div>
+                        <p className='text-[#4B5A54] text-xs md:text-sm'>avaliação média</p>
+                    </div>
+                    <div className='flex flex-col gap-2 justify-center items-center '>
+                        <p className='font-bold'>24/7</p>
+                        <p className='text-[#4B5A54] text-xs md:text-sm'>suporte via chat</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- landing-page/src/components/Navbar/index.tsx
+++ landing-page/src/components/Navbar/index.tsx
@@ -3,14 +3,13 @@ import {LuCalendarDays} from 'react-icons/lu'
 export default function Navbar() {
     return (
         <header className='md:px-20 px-5 py-6 flex justify-between items-center border-b border-b-[#ccc]'>
-            <div className='flex gap-2'>
+            <div className='flex gap-2 items-center'>
                 <MdOutlinePets size={24} color="#3F9271" />
-                <p className='font-black text-[#153229] text-sm md:text-base'>pet</p>
-                <p className='font-black text-[#FF6B4A] text-sm md:text-base'>care</p>
+                <span className='font-black text-[#153229] text-xs md:text-xl'>pet</span>
+                <span className='font-black text-[#FF6B4A] text-xs md:text-xl'>care</span>
             </div>
-            <nav>
-
-                <ul className='flex items-center gap-5 md:gap-10'>
+            <nav className=''>
+                <ul className='flex gap-2 md:gap-8'>
                     <li>
                         <a href='' className='text-[#1F4136] text-xs md:text-base'>Início</a>
                     </li>
@@ -23,7 +22,7 @@ export default function Navbar() {
                 </ul>
             </nav>
 
-            <a href='' className='hidden md:block bg-[#1F4136] text-white rounded-full font-medium text-sm px-6 py-3 shadow-2xl'>
+            <a href='' className=' hidden md:block bg-[#1F4136] text-white rounded-full font-medium text-xs px-6  py-3 shadow-2xl'>
                 Agendar Consulta
             </a>
             <div className='block md:hidden bg-[#153229] p-2 rounded-full'>
