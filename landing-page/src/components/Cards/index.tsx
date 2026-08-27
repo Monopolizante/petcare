680b183f0bf6395f0005d96fc3657528341dc9d4
Monopolizante <robsonlaurensan@gmail.com>
2026-08-26 22:01:22 -0300

    bugfix: corrige os parâmetros do componente Cards

--- landing-page/src/components/Cards/index.tsx
+++ landing-page/src/components/Cards/index.tsx
@@ -3,17 +3,18 @@ interface CardProps {
     title: string;
     info: string;
     icon: React.ReactElement;
+    color: string;
 }
 
-export default function Cards ({title, info, icon}: CardProps){
+export default function Cards ({title, info, icon, color}: CardProps){
     return(
         
             <div className='bg-white rounded-lg w-md p-5 m-3 shadow-xs'>
-                <div className='items-center bg-[#DCEFE4] inline-flex p-2 mt-3 mb-3 rounded-lg'>
+                <div className={`items-center bg-[${color}] inline-flex p-2 mt-3 mb-3 rounded-lg`}>
                     {icon}
                 </div>
                     <h1 className='font-black text-3xl'>{title}</h1>
-                <p className='text-wrap mt-4 mb-6 text-[#4B5A54] text-sm md:text-base text-left'>{info}</p>
+                <p className={`text-wrap mt-4 mb-6 text-[${color}] text-sm md:text-base text-left`}>{info}</p>
             </div>
     )
 }
